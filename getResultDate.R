f3fabb2b883e0aff30ae640e9139dbcbbf3c3ad5
cimoody <cristina.isadora.moody@gmail.com>
2015-07-08 15:30:50 -0400

    Function getResultDate that plots lab results by date for each patient with repeated labs above a cutoff. Function returns a list of patient dataframes with lab result and date.

---
+++ getResultDate.R
@@ -0,0 +1,159 @@
+## getResultDate.R
+## Written by Cristina Moody
+## July 2015
+
+# Installing Gaussian Process Library
+install.packages("tgp");
+library(tgp);
+# Installing package to compare the two data tables
+install.packages("compare");
+library(compare);
+install.packages("plyr");
+library(plyr);
+
+# Preprosessing files - must be done manually for each data set
+# Working directory: "C:/Users/CMoody/Desktop/workspace/VNL"
+wDir <- getwd();
+# Data directory: E:/VNL Data from Joe
+dDir <- "E:/VNL\ Data\ from\ Joe/";
+# Data filenames
+data.popname = sprintf("%s%s", dDir, "di_populations_v1.1.csv");
+data.labname = sprintf("%s%s", dDir, "di_lab_data_v1.1.csv");
+data.pop <- read.csv(file = data.popname, header = TRUE, stringsAsFactors = FALSE);
+data.lab <- read.csv(file = data.labname, header = TRUE, stringsAsFactors = FALSE);
+# Saving the data in R for manipulation
+save(data.pop, file = sprintf("%s%s", dDir, "dataPop.rda"));
+save(data.lab, file = sprintf("%s%s", dDir, "dataLab.rda"));
+# Checking columns for NA
+if( sum( is.na(data.lab$X) + 0 ) >= length( data.lab$X ) ) {
+    print(" X is all NA\n");
+} # from D. Sanchez
+if( sum( is.na(data.pop$X) + 0 ) >= length( data.pop$X ) ) {
+    print(" X is all NA\n");
+} # from D. Sanchez
+if( sum( is.na(data.pop$X.1) + 0 ) >= length( data.pop$X.1 ) ) {
+    print(" X is all NA\n");
+} # from D. Sanchez
+drops <- c("X");
+goodlab <- data.lab[,!(names(data.lab) %in% drops)];
+drops <- c("X", "X.1");
+goodpop <- data.pop[,!(names(data.pop) %in% drops)];
+# Save modified files for R manipulation
+save(goodpop, goodlab, file = sprintf("%s%s", dDir, "goodData.rda"));
+list.files(path = dDir, pattern = "\\.rda");
+load(sprintf("%s%s", dDir, "goodData.rda"));
+
+# Choosing 80048_1518 as first test to evaluate
+tID <- c("CPT_CODE"="80048", "COMPONENT_ID"= as.numeric(1523));
+# Choosing cutoff
+cutoff <- 20;
+
+## FUNCTION THAT RETURNS LAB RESULT AND ORDERING DATA FOR ANY GIVEN LAB
+getResultDate <- function(cutoff, tID){
+    # Make a variable that is a combination of ENC_CSN_ID, CPT_CODE, COMPONENT_ID
+    # CPT_CODE & COMPONENT_ID are the lab
+    labnames <- paste(goodlab$ENC_CSN_ID, goodlab$CPT_CODE, goodlab$COMPONENT_ID, sep = "_");
+    # Use table to get the frequency of each combined patient & lab
+    topLabs <- table(labnames);
+    # Order these by frequency
+    df <- topLabs;
+    df <- df[order(df)];
+    # cutoff <- 50;
+    dfNotRare <- df[df>=cutoff];
+    # Determining which labs pass cutoff <= 50;
+    # Accessing the row names in the table (since they are ID_lab)
+    rn <- row.names(dfNotRare);
+    rn <- strsplit(rn, "_"); # Splitting up the names into the components
+    # Transposing the list rn
+    lis <- rn;
+    m <- do.call(rbind, lis);
+    split(m, col(m));
+    rn2 <- m;
+    gtCOtests <- data.frame(rn2); # Creating a data frame for ease of use
+    # Combining the number of tests with the patient and lab information
+    gtCOtests <- cbind(gtCOtests, dfNotRare);
+    colnames(gtCOtests) <- c("ENC_CSN_ID", "CPT_CODE",
+                             "COMPONENT_ID", "TIMES_TEST_REPEATED");
+    gtCOtests$CPT_ID_CODE = paste(gtCOtests$CPT_CODE, gtCOtests$COMPONENT_ID, sep = "_");
+    # Finding which tests patients (determined by ENC_CSN_ID) had more than cutoff # of times
+    whTestsgtCO <- table(gtCOtests$CPT_ID_CODE);
+    whTestsgtCO <- whTestsgtCO[order(whTestsgtCO)];
+    # # Choosing 80048_1518 as first test to evaluate
+    # tID <- c("CPT_CODE"="80048", "COMPONENT_ID"= as.numeric(1519));
+    # ENC_CSN_ID from
+    firstENC <- gtCOtests[gtCOtests$COMPONENT_ID==as.numeric(tID[2]) &
+                              gtCOtests$CPT_CODE==tID[1],];
+    # ENC_CSN_ID of pateintw with specific test
+    v <- firstENC$ENC_CSN_ID; # Vector of ENC_CSN_ID want to plot
+    labID <- tID;
+    # Function to get specific ENC_CSN_ID lab results for specific lab
+    getPat <- function(vec, labID. = labID) {
+        pat <- goodlab[(goodlab$CPT_CODE==labID.[1] &
+                            goodlab$COMPONENT_ID==as.numeric(labID.[2]) &
+                            goodlab$ENC_CSN_ID==vec),];
+        return(pat);
+    }
+    # Getting lab results for specific lab
+    pat <- list(lapply(v, FUN = getPat));
+    # Isolatinge the ORDERING_DATE
+    trTimeLAB <- list(c(length(v)));
+    for (i in 1:length(v)){
+        trTimeLAB[[i]] <- as.Date(pat[[1]][i][[1]]$ORDERING_DATE,
+                                  format = "%m/%d/%Y %H:%M");
+    }
+    # Isolating lab ORD_NUM_VALUE
+    patLAB <- list(c(length(v)));
+    for (i in 1:length(v)){
+        patLAB[[i]] <- as.numeric(as.character(pat[[1]][i][[1]]$ORD_NUM_VALUE));
+    }
+    # Creating tables for each ENC_CSN_ID of ORDERING_DATE & ORD_NUM_VALUE in a list
+    patTLAB <- list(c(length(v)));
+    for (i in 1:length(v)){
+        patTLAB[[i]] <- data.frame(Test_Order_Date = trTimeLAB[i], LAB = patLAB[i]);
+        names(patTLAB[[i]]) <- c("ORDERING_DATE", "ORD_NUM_VALUE");
+    }
+    # Removing invalid results of 99999
+    for (i in 1:length(v)){
+        patTLAB[[i]] <- patTLAB[[i]][patTLAB[[i]]$ORD_NUM_VALUE < 77777, ];
+    }
+    # Ordering chronologically
+    for (i in 1:length(v)){
+        patTLAB[[i]] <- patTLAB[[i]][order(patTLAB[[i]]$ORDERING_DATE), ];
+    }
+    # Setting up graphics
+    ymax <- c(); ymin <- c();
+    for(i in 1:length(v)){
+        ymax[i] <- max(patTLAB[[i]][, 2]);
+        ymin[i] <- min(patTLAB[[i]][, 2]);
+    }
+    co <- rainbow(length(v));
+    plot(patTLAB[[i]]$ORDERING_DATE, patTLAB[[i]]$ORD_NUM_VALUE,
+         col = co[i], type = "o", panel.first = grid(),
+         xlim = range(as.Date("2008-02-01"),as.Date("2008-11-30")),
+         ylim = range(min(ymin), max(ymax)),
+         xlab = "Time (days)",
+         ylab = sprintf("Lab %s_%s   (%s)", pat[[1]][1][[1]]$CPT_CODE[1],
+                        pat[[1]][1][[1]]$COMPONENT_ID[1],
+                        pat[[1]][1][[1]]$REFERENCE_UNIT[1]));
+    for (i in 2:length(v)){
+        lines(patTLAB[[i]]$ORDERING_DATE, patTLAB[[i]]$ORD_NUM_VALUE,
+              col = co[i], type = "b", panel.first = grid(),
+              xlim = range(as.Date("2008-02-01"),as.Date("2008-11-30")),
+              ylim = range(min(ymin), max(ymax)),
+              xlab = "Time (days)",
+              ylab = sprintf("Lab %s_%s   (%s)", pat[[1]][1][[1]]$CPT_CODE[1],
+                             pat[[1]][1][[1]]$COMPONENT_ID[1],
+                             pat[[1]][1][[1]]$REFERENCE_UNIT[1]));
+        axis.Date(side = 1, x = as.Date(patTLAB[[i]]$ORDERING_DATE))}
+    #     plot(0, 0, panel.first = grid(),
+    #          xlim = range(as.Date("2008-02-01"),as.Date("2008-11-30")), ylim = range(100, 170),
+    #          xlab = "Time (days)", ylab = "Blood Urea Nitrogen level (mmol/L)");
+    #     for (i in 1:length(v)){
+    #         lines(patTLAB[[i]]$ORDERING_DATE, patTLAB[[i]]$ORD_NUM_VALUE,
+    #               col = co[i], type = "b");}
+    return(patTLAB);
+}
+
+
+
+
