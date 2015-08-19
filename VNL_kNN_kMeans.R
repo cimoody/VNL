319cbf23ff2b6dce0d800d0fe43d2b83904aad64
cimoody <cristina.isadora.moody@gmail.com>
2015-08-19 14:07:53 -0400

    Commented out testing code in createMeta().

--- VNL_kNN_kMeans.R
+++ VNL_kNN_kMeans.R
@@ -41,135 +41,135 @@ createMeta <- function(mDF){
     mergedDFg <- mDF;
     # Fixing CPT_CODE
     mergedDFg$CPT_CODE <- factor(mergedDFg$CPT_CODE, labels = c("80048", "80053.01", "80069", "CBCD", "85027"));
-    summary(mergedDFg$CPT_CODE);
-    summary(table(mergedDFg$CPT_CODE));
+#     summary(mergedDFg$CPT_CODE);
+#     summary(table(mergedDFg$CPT_CODE));
     # Fixing REFERENCE_UNIT
-    str(mergedDFg);
-    summary(table(mergedDFg$REFERENCE_UNIT));
-    summary((mergedDFg$REFERENCE_UNIT));
-    class(mergedDFg$REFERENCE_UNIT);
+#     str(mergedDFg);
+#     summary(table(mergedDFg$REFERENCE_UNIT));
+#     summary((mergedDFg$REFERENCE_UNIT));
+#     class(mergedDFg$REFERENCE_UNIT);
     mergedDFg$REFERENCE_UNIT <- factor(mergedDFg$REFERENCE_UNIT, labels = c("mg/dL", "mmol/L", "K/uL"));
     # Fixing REFERENCE_LOW
-    summary(table(mergedDFg$REFERENCE_LOW));
-    summary((mergedDFg$REFERENCE_LOW));
-    class(mergedDFg$REFERENCE_LOW);
+#     summary(table(mergedDFg$REFERENCE_LOW));
+#     summary((mergedDFg$REFERENCE_LOW));
+#     class(mergedDFg$REFERENCE_LOW);
     mergedDFg$REFERENCE_LOW <- factor(mergedDFg$REFERENCE_LOW, labels = c("6", "0.7", "3.5", "135", "150", "4"));
     mergedDFg$REFERENCE_LOW <- f2n(mergedDFg$REFERENCE_LOW)
     # Fixing REFERENCE_HIGH
-    summary(table(mergedDFg$REFERENCE_HIGH));
-    summary((mergedDFg$REFERENCE_HIGH));
-    class(mergedDFg$REFERENCE_HIGH);
+#     summary(table(mergedDFg$REFERENCE_HIGH));
+#     summary((mergedDFg$REFERENCE_HIGH));
+#     class(mergedDFg$REFERENCE_HIGH);
     mergedDFg$REFERENCE_HIGH <- f2n(mergedDF$REFERENCE_HIGH)
     # Fixing ETHNIC_GROUP_C
-    str(mergedDFg$ETHNIC_GROUP_C);
-    summary(table(mergedDFg$ETHNIC_GROUP_C));
-    summary((mergedDFg$ETHNIC_GROUP_C));
-    class(mergedDFg$ETHNIC_GROUP_C);
+#     str(mergedDFg$ETHNIC_GROUP_C);
+#     summary(table(mergedDFg$ETHNIC_GROUP_C));
+#     summary((mergedDFg$ETHNIC_GROUP_C));
+#     class(mergedDFg$ETHNIC_GROUP_C);
     mergedDFg$ETHNIC_GROUP_C <- as.numeric(mergedDF$ETHNIC_GROUP_C);
     # Fixing PCP_PROV_ID
     mergedDFg$PCP_PROV_ID <- as.numeric(mergedDF$PCP_PROV_ID);
     # Fixing CO_CD
-    str(mergedDFg$CO_CD);
-    summary(table(mergedDFg$CO_CD));
-    summary((mergedDFg$CO_CD));
-    class(mergedDFg$CO_CD);
+#     str(mergedDFg$CO_CD);
+#     summary(table(mergedDFg$CO_CD));
+#     summary((mergedDFg$CO_CD));
+#     class(mergedDFg$CO_CD);
     mergedDFg$CO_CD <- as.factor(mergedDF$CO_CD); # Can remove this column all the same
     # Fixing BILL_ADMS_DT
-    str(mergedDFg$BILL_ADMS_DT);
-    (table(mergedDFg$BILL_ADMS_DT));
-    summary((mergedDFg$BILL_ADMS_DT));
-    class(mergedDFg$BILL_ADMS_DT);
+#     str(mergedDFg$BILL_ADMS_DT);
+#     (table(mergedDFg$BILL_ADMS_DT));
+#     summary((mergedDFg$BILL_ADMS_DT));
+#     class(mergedDFg$BILL_ADMS_DT);
     mergedDFg$BILL_ADMS_DT <- mdy(mergedDF$BILL_ADMS_DT, tz = "EST");
     # Fixing BILL_DISCHRG_DT
-    str(mergedDFg$BILL_DISCHRG_DT);
-    (table(mergedDFg$BILL_DISCHRG_DT));
-    summary((mergedDFg$BILL_DISCHRG_DT));
-    class(mergedDFg$BILL_DISCHRG_DT);
+#     str(mergedDFg$BILL_DISCHRG_DT);
+#     (table(mergedDFg$BILL_DISCHRG_DT));
+#     summary((mergedDFg$BILL_DISCHRG_DT));
+#     class(mergedDFg$BILL_DISCHRG_DT);
     mergedDFg$BILL_DISCHRG_DT <- mdy(mergedDF$BILL_DISCHRG_DT, tz = "EST");
     # Fixing READMT_WITHIN_30_DAYS
-    str(mergedDFg$READMT_WITHIN_30_DAYS);
-    (table(mergedDFg$READMT_WITHIN_30_DAYS));
-    summary((mergedDFg$READMT_WITHIN_30_DAYS));
-    class(mergedDFg$READMT_WITHIN_30_DAYS);
+#     str(mergedDFg$READMT_WITHIN_30_DAYS);
+#     (table(mergedDFg$READMT_WITHIN_30_DAYS));
+#     summary((mergedDFg$READMT_WITHIN_30_DAYS));
+#     class(mergedDFg$READMT_WITHIN_30_DAYS);
     mergedDFg$READMT_WITHIN_30_DAYS <- as.factor(mergedDF$READMT_WITHIN_30_DAYS);
     # Fixing ADMT_DIAG
-    str(mergedDFg$ADMT_DIAG);
-    (table(mergedDFg$ADMT_DIAG));
-    summary((mergedDFg$ADMT_DIAG));
-    class(mergedDFg$ADMT_DIAG);
+#     str(mergedDFg$ADMT_DIAG);
+#     (table(mergedDFg$ADMT_DIAG));
+#     summary((mergedDFg$ADMT_DIAG));
+#     class(mergedDFg$ADMT_DIAG);
     mergedDFg$ADMT_DIAG <- as.factor(mergedDF$ADMT_DIAG);
     # Fixing ADMT_NURSS_STTN
-    str(mergedDFg$ADMT_NURSS_STTN);
-    (table(mergedDFg$ADMT_NURSS_STTN));
-    summary((mergedDFg$ADMT_NURSS_STTN));
-    class(mergedDFg$ADMT_NURSS_STTN);
+#     str(mergedDFg$ADMT_NURSS_STTN);
+#     (table(mergedDFg$ADMT_NURSS_STTN));
+#     summary((mergedDFg$ADMT_NURSS_STTN));
+#     class(mergedDFg$ADMT_NURSS_STTN);
     mergedDFg$ADMT_NURSS_STTN <- as.factor(mergedDF$ADMT_NURSS_STTN);
     # Fixing ADMT_SRC
-    str(mergedDFg$ADMT_SRC);
-    (table(mergedDFg$ADMT_SRC));
-    summary((mergedDFg$ADMT_SRC));
-    class(mergedDFg$ADMT_SRC);
+#     str(mergedDFg$ADMT_SRC);
+#     (table(mergedDFg$ADMT_SRC));
+#     summary((mergedDFg$ADMT_SRC));
+#     class(mergedDFg$ADMT_SRC);
     mergedDFg$ADMT_SRC <- as.factor(mergedDF$ADMT_SRC);
     # Fixing ADMT_TYP
-    str(mergedDFg$ADMT_TYP);
-    summary(table(mergedDFg$ADMT_TYP));
-    summary((mergedDFg$ADMT_TYP));
-    class(mergedDFg$ADMT_TYP);
+#     str(mergedDFg$ADMT_TYP);
+#     summary(table(mergedDFg$ADMT_TYP));
+#     summary((mergedDFg$ADMT_TYP));
+#     class(mergedDFg$ADMT_TYP);
     mergedDFg$ADMT_TYP <- as.factor(mergedDF$ADMT_TYP);
     # Fixing PRINC_PROC
-    str(mergedDFg$PRINC_PROC);
-    summary(table(mergedDFg$PRINC_PROC));
-    summary((mergedDFg$PRINC_PROC));
-    class(mergedDFg$PRINC_PROC);
+#     str(mergedDFg$PRINC_PROC);
+#     summary(table(mergedDFg$PRINC_PROC));
+#     summary((mergedDFg$PRINC_PROC));
+#     class(mergedDFg$PRINC_PROC);
     mergedDFg$PRINC_PROC <- as.numeric(mergedDF$PRINC_PROC);
     # Fixing REFRL_SRC
-    str(mergedDFg$REFRL_SRC);
-    summary(table(mergedDFg$REFRL_SRC));
-    summary((mergedDFg$REFRL_SRC));
-    class(mergedDFg$REFRL_SRC);
+#     str(mergedDFg$REFRL_SRC);
+#     summary(table(mergedDFg$REFRL_SRC));
+#     summary((mergedDFg$REFRL_SRC));
+#     class(mergedDFg$REFRL_SRC);
     mergedDFg$REFRL_SRC <- as.factor(mergedDF$REFRL_SRC);
     # Fixing REFRG_MD
-    str(mergedDFg$REFRG_MD);
-    summary(table(mergedDFg$REFRG_MD));
-    summary((mergedDFg$REFRG_MD));
-    class(mergedDFg$REFRG_MD);
+#     str(mergedDFg$REFRG_MD);
+#     summary(table(mergedDFg$REFRG_MD));
+#     summary((mergedDFg$REFRG_MD));
+#     class(mergedDFg$REFRG_MD);
     mergedDFg$REFRG_MD <- as.numeric(mergedDF$REFRG_MD);
     # Fixing APR_DRG_CD
-    str(mergedDFg$APR_DRG_CD);
-    summary(table(mergedDFg$APR_DRG_CD));
-    summary((mergedDFg$APR_DRG_CD));
-    class(mergedDFg$APR_DRG_CD);
+#     str(mergedDFg$APR_DRG_CD);
+#     summary(table(mergedDFg$APR_DRG_CD));
+#     summary((mergedDFg$APR_DRG_CD));
+#     class(mergedDFg$APR_DRG_CD);
     mergedDFg$APR_DRG_CD <- as.numeric(mergedDF$APR_DRG_CD);
     # Fixing APR_SEV_LVL
-    str(mergedDFg$APR_SEV_LVL);
-    summary(table(mergedDFg$APR_SEV_LVL));
-    summary((mergedDFg$APR_SEV_LVL));
-    class(mergedDFg$APR_SEV_LVL);
+#     str(mergedDFg$APR_SEV_LVL);
+#     summary(table(mergedDFg$APR_SEV_LVL));
+#     summary((mergedDFg$APR_SEV_LVL));
+#     class(mergedDFg$APR_SEV_LVL);
     mergedDFg$APR_SEV_LVL <- as.numeric(mergedDF$APR_SEV_LVL);
     # Fixing DISCHRG_DEPT
-    str(mergedDFg$DISCHRG_DEPT);
-    summary(table(mergedDFg$DISCHRG_DEPT));
-    summary((mergedDFg$DISCHRG_DEPT));
-    class(mergedDFg$DISCHRG_DEPT);
+#     str(mergedDFg$DISCHRG_DEPT);
+#     summary(table(mergedDFg$DISCHRG_DEPT));
+#     summary((mergedDFg$DISCHRG_DEPT));
+#     class(mergedDFg$DISCHRG_DEPT);
     mergedDFg$DISCHRG_DEPT <- as.factor(mergedDF$DISCHRG_DEPT);
     # Fixing DISCHRG_DISPOS
-    str(mergedDFg$DISCHRG_DISPOS);
-    summary(table(mergedDFg$DISCHRG_DISPOS));
-    summary((mergedDFg$DISCHRG_DISPOS));
-    class(mergedDFg$DISCHRG_DISPOS);
+#     str(mergedDFg$DISCHRG_DISPOS);
+#     summary(table(mergedDFg$DISCHRG_DISPOS));
+#     summary((mergedDFg$DISCHRG_DISPOS));
+#     class(mergedDFg$DISCHRG_DISPOS);
     mergedDFg$DISCHRG_DISPOS <- as.factor(mergedDF$DISCHRG_DISPOS);
 
     # Columns to remove from classification
     removCol <- c("MIN_RAW_LABS", "CO_CD");
 
-    str(mergedDFg)
+    # str(mergedDFg);
     meta <- subset(mergedDFg, select=-c(MIN_RAW_LABS, CO_CD));
     return(meta);
 }
 
 meta <- createMeta(mergedDF)
 
-# NEED to source(VNL_graph.R) to get table meta below.
+# NEED to source(VNL_graph.R) to get table meta below or use createMeta() above
 # Sorting into testing and training sets
 n.points <- nrow(meta);
 sampling.rate <- 0.8;
@@ -394,7 +394,7 @@ points(meta2$THRESHOLD_TIME, classifier3$predictions, pch = meta2$INT_FLAG+22, p
 
 ## CLASSIFYING FOR INT_FLAG
 # Trying binary classifier to predict if patient belongs to INT_FLAG==1 category.
-flag.cut <- 0.515;
+flag.cut <- 0.515; # Because classifier returns a continous value between 0-1, not 0 or 1
 vnl.knn.flag <- train.kknn(formula = formula(`INT_FLAG` ~ .),
                       data = vnl.train, kmax = 50, distance = 1);
 vnl.knn.flag;
@@ -457,7 +457,7 @@ findbest.flag.cut[which.max(findbest.flag.cut$predict.accuracy),];
 # Again, finer
 findbest.flag.cut <- data.frame(flag.cut = numeric(0),
                                 test.accuracy = numeric(0), predict.accuracy = numeric(0));
-for (j in seq(0.45, 0.65, 0.005)){
+for (j in seq(0.45, 0.65, 0.005)){ # finding best value for flag.cut between 0.45 and 0.65
     print(j); flag.cut <- j;
     vnl.knn.flag1 <- vnl.knn.flag$fitted.values[[21]][1:356];
     vnl.knn.flag1 <-  vapply(vnl.knn.flag1, FUN = function(x){if (x > flag.cut) 1 else 0}, FUN.VALUE = c(1));
